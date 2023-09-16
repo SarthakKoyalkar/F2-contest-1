4da3b40039bf3826dd8ba3e54cc6f01406b3f4f4
sl119033373882 <sarthakkoyalkar000@gmail.com>
2023-09-16 20:13:54 +0530

    final

--- script.js
+++ script.js
@@ -4,31 +4,111 @@ const data = [
 ];
 
 // 1. Print Developers
-function printDeveloper() {}
+function printDeveloper() {
+  for( i in data)
+  {
+    
+    console.log(data[i]);
+  }
+  
+}
 
 // 2. Add Data
-function addData() {}
+function addData() {
+  
+
+}
 
 // 3. Remove Admins
-function removeAdmin() {}
+function removeAdmin() {
+  
+  for (i in data)
+  {
+    if(data[i].profession == "admin")
+    {
+      delete data[i];
+    }
+  }
+  console.log(data);
+}
 
 // 4. Concatenate Array
-function concatenateArray() {}
+function concatenateArray() {
+  const data2 =[{name: "sarthak", age: 22, profession: "Jdeveloper" },
+  {name: "vaibhav", age: 22,profession: "Javadeveloper" }]
+
+  let concatenatedArray = data.concat(data2);
+  console.log(concatenatedArray);
+}
 
 // 5. Average Age
-function averageAge() {}
+function averageAge() {
+  let sum=0;
+  for (i in data)
+  {
+    sum = data[i].age + sum;
+  }
+  console.log(sum/data.length) ;
+}
 
 // 6. Age Check
-function checkAgeAbove25() {}
+function checkAgeAbove25() {
+  for(i in data)
+  {
+    if(data[i].age>25)
+    {
+      console.log("yes a person with age more than 25 is present.")
+    }
+    else
+    console.log("no person with age above 25");
+  }
+}
 
 // 7. Unique Professions
-function uniqueProfessions() {}
+function uniqueProfessions() {
+  let array1 = [];
+  for ( i in data)
+  {
+    array1.push(data[i].profession);
+  }
+  let outputArray = array1.filter(function (v, i, self) {
+ 
+    // It returns the index of the first
+    // instance of each value
+    return i == self.indexOf(v);
+});
+    console.log(outputArray);
+}
 
 // 8. Sort by Age
-function sortByAge() {}
+function sortByAge() {
+  
+}
 
 // 9. Update Profession
-function updateJohnsProfession() {}
+function updateJohnsProfession() {
+  for(i in data)
+  {
+    if(data[i].name == "john")
+    {
+      data[i].profession = "manager";
+    }
+  }
+  console.log(data);
+}
 
 // 10. Profession Count
-function getTotalProfessions() {}
+function getTotalProfessions() {
+  let array1 = [];
+  for ( i in data)
+  {
+    array1.push(data[i].profession);
+  }
+  let outputArray = array1.filter(function (v, i, self) {
+ 
+    // It returns the index of the first
+    // instance of each value
+    return i == self.indexOf(v);
+});
+console.log(outputArray.length);
+}
